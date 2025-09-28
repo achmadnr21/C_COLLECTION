a35b199f9795dbfd47fe563724236daf45732cc0
Achmad Nashruddin Riskynanda <achmad.riskynanda@traveloka.com>
2025-09-28 13:47:54 +0700

    feat: input simulation

--- rudy_englibs/input_response_mapping/main.c
+++ rudy_englibs/input_response_mapping/main.c
@@ -8,10 +8,15 @@
 
 
 int main() {
-    input_response_mapping_t pitch, roll, yaw;
-    INPUT_RESPONSE_MAPPING_init(&pitch, 1.0f, 1.0f, 0.05f, 0.0f, 0.5f);
+    input_response_mapping_t roll;
     INPUT_RESPONSE_MAPPING_init(&roll, 1.0f, 1.0f, 0.05f, 0.0f, 0.5f);
-    INPUT_RESPONSE_MAPPING_init(&yaw, 1.0f, 1.0f, 0.05f, 0.0f, 0.5f);
+    roll.info(&roll);
+
+    // simulate input from -1.0 to 1.0
+    for (float input = -1.0f; input <= 1.0f; input += 0.01f) {
+        float response = roll.get_response(&roll, input);
+        printf("j_input: %5.2f\t|sig_resp: %5.2f\n", input, response);
+    }
 
     return 0;
 }
