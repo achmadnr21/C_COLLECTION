e278d7b28c7082469a11bf0b2bf6199c07bbf5ea
Achmad Nashruddin Riskynanda <achmad.riskynanda@traveloka.com>
2025-11-08 16:34:29 +0700

    fix: main function demo scenario

--- rudy_englibs/threadpool_process/main.c
+++ rudy_englibs/threadpool_process/main.c
@@ -8,15 +8,10 @@
 
 #include "lib/rudy_threadpool.h"
 
-void pdelay(int seconds) {
-    clock_t start_time = clock();
-    while (clock() < start_time + seconds * CLOCKS_PER_SEC);
-}
-
 void* my_task_function(void* arg) {
     int task_id = (int)(intptr_t)arg;
-    pdelay(20);
-    printf("Thread ID: %lu\nExecuting Task %d\n", (unsigned long)pthread_self(), task_id);
+    sleep(2);
+    printf("Thread ID: %lu == > Task %d ==> Execution Complete\n", (unsigned long)pthread_self(), task_id);
     return NULL;
 }
 
@@ -24,28 +19,27 @@ void* my_task_function(void* arg) {
 int main() {
 
     thread_pool_t pool;
-    THREADPOOL_init(
-        &pool,
-        32,
-        40);
+    THREADPOOL_init(&pool, 5, 40);
 
     // Add tasks to the pool
-    uint64_t first_task_id = 0;
-    for (int i = 0; i < 20; i++) {
+    
+    const int total_tasks = 20;
+    uint64_t task_ids[total_tasks];
+    for (int i = 0; i < total_tasks; i++) {
         uint64_t task_id = pool.add_task(&pool, my_task_function, (void *)(intptr_t)i);
-        if(i == 0) {
-            first_task_id = task_id;
-        }
         if (task_id == 0) {
             fprintf(stderr, "Failed to add task %d\n", i);
         }
+        task_ids[i] = task_id;
         printf("Added Task %d with ID %llu\n", i, task_id);
     }
-
-    printf("\nChecking state of first task (ID: %llu):\n", first_task_id+1);
-    task_state_enum_t state = pool.get_task_state(&pool, first_task_id+1);
-    printf("State of Task ID %llu: %s\n", first_task_id+1, pool.task_state_to_string(&pool,state));
-
+    // loop checking all task states
+    sleep(3);
+    for (int i = 0; i < total_tasks; i++) {
+        task_state_enum_t state = pool.get_task_state(&pool, task_ids[i]);
+        printf("State of Task ID %llu: %s\n", task_ids[i], pool
+            .task_state_to_string(&pool,state));
+    }
     // Destroy the pool
     pool.destroy(&pool);
     return 0;
